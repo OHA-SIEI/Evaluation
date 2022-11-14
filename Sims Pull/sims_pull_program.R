c27c47b478735f7a162ef1283cf64bd94cec0733
rcarrollusaid <111500994+rcarrollusaid@users.noreply.github.com>
2022-11-14 11:28:15 -0700

    Add files via upload

---
+++ Sims Pull/sims_pull_program.R
@@ -0,0 +1,13 @@
+# library(knitr)
+# library(rmarkdown)
+# 
+ setwd("C:/Users/rcarroll/Desktop/Git/Evaluation/Sims Pull")
+# 
+# 
+for (i in unique(sims_mock$OU_Name)) {
+  rmarkdown::render(input = "SIMS_High_level_analysis_pull.Rmd",
+                    params = list(OU_Name = "Burundi"),
+                    output_format = "pdf_document",
+                    output_file = paste0(i, ".pdf"))
+}
+
