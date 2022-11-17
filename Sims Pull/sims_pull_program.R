e104a9d42ffdc5a49d8c4b8e108264bb17b341fc
rcarrollusaid <111500994+rcarrollusaid@users.noreply.github.com>
2022-11-17 08:12:10 -0700

    Add files via upload

--- Sims Pull/sims_pull_program.R
+++ Sims Pull/sims_pull_program.R
@@ -6,7 +6,7 @@
 # 
 for (i in unique(sims_mock$OU_Name)) {
   rmarkdown::render(input = "SIMS_High_level_analysis_pull.Rmd",
-                    params = list(OU_Name = "Burundi"),
+                    params = list(OU_Name = i),
                     output_format = "pdf_document",
                     output_file = paste0(i, ".pdf"))
 }
