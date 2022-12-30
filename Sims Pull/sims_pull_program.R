f1ae31dc5f6abf6b2e45f90328430adb8e043320
jperng <jperng@usaid.gov>
2022-12-30 13:30:52 -0500

    Integrating in code to the Rmd file to loop over OUs.

--- Sims Pull/sims_pull_program.R
+++ Sims Pull/sims_pull_program.R
@@ -4,9 +4,13 @@
  setwd("C:/Users/rcarroll/Desktop/Git/Evaluation/Sims Pull")
 # 
 # 
-for (i in unique(sims_mock$OU_Name)) {
+#for (i in unique(sims_mock$OU_Name)) {
+ for (i in unique(SIMSdatain$OU.Name)) {
+   
+  
   rmarkdown::render(input = "SIMS_High_level_analysis_pull.Rmd",
-                    params = list(OU_Name = i),
+                    #params = list(OU_Name = i),
+                    params = list(OU.Name = i),
                     output_format = "pdf_document",
                     output_file = paste0(i, ".pdf"))
 }
