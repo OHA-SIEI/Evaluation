a12d103bf8aa9e242ebc6930bf2ad5c7ea890dcc
jpusaid <109992572+jpusaid@users.noreply.github.com>
2023-01-04 13:20:50 -0500

    Cleaned SIMS data, added OU-spec analysis

--- Sims Pull/sims_pull_program.R
+++ Sims Pull/sims_pull_program.R
@@ -1,14 +1,18 @@
 # library(knitr)
 # library(rmarkdown)
 # 
- setwd("C:/Users/rcarroll/Desktop/Git/Evaluation/Sims Pull")
+ #setwd("C:/Users/rcarroll/Desktop/Git/Evaluation/Sims Pull")
+library(here)
+
+setwd(here())
+
 # 
 # 
 #for (i in unique(sims_mock$OU_Name)) {
  for (i in unique(SIMSdatain$OU.Name)) {
    
   
-  rmarkdown::render(input = "SIMS_High_level_analysis_pull.Rmd",
+  rmarkdown::render(input = "SIMS_High_level_analysis_pull_v2.Rmd",
                     #params = list(OU_Name = i),
                     params = list(OU.Name = i),
                     output_format = "pdf_document",
