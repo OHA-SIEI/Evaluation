a0978eddb626af7cf6f75f9f428805abe78310eb
jpusaid <109992572+jpusaid@users.noreply.github.com>
2023-01-09 17:53:13 -0500

    cleaning and formatting
    Made more changes to content, formatting, and data cleaning.

--- Sims Pull/sims_pull_program.R
+++ Sims Pull/sims_pull_program.R
@@ -6,18 +6,18 @@ library(here)
 
 setwd(here())
 
-# 
+# First, you may have to run the first chunk to be able to go through this loop
 # 
 #for (i in unique(sims_mock$OU_Name)) {
  for (i in unique(SIMSdatain$OU.Name)) {
    
   
-  rmarkdown::render(input = "SIMS_High_level_analysis_pull_v2.Rmd",
+  rmarkdown::render(input = "SIMS_High_level_analysis_pull_v4.Rmd",
                     #params = list(OU_Name = i),
                     params = list(OU.Name = i),
-                    output_format = "word_document",
-                    #output_format = "pdf_document",
-                    output_file = paste0(i, ".doc"))
-  # output_file = paste0(i, ".pdf"))
+                   # output_format = "pdf_document",
+                   #  output_file = paste0(i, ".pdf")
+                   output_format = "word_document",
+                   output_file = paste0(i, "SIMS.doc"))
 }
 
